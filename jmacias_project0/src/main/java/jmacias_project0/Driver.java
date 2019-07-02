751688e22c00fb3c1fa0a9895e53af07dbf0564c
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-07-01 20:42:15 -0400

    Set up driver to accept signin/login choice

--- jmacias_project0/src/main/java/jmacias_project0/Driver.java
+++ jmacias_project0/src/main/java/jmacias_project0/Driver.java
@@ -1,10 +1,51 @@
 package jmacias_project0;
 
+import java.io.*;
+
 public class Driver {
+	
+	private static InputStreamReader iReader = new InputStreamReader(System.in);
+	private static BufferedReader bReader = new BufferedReader(iReader);
+	
+	private static String welcome() {
+		System.out.println("     Please enter the number of your command");
+		System.out.println("1. Login\n2. Signup");
+		
+		try {
+			String command = bReader.readLine();
+			return command;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+	
+	private static void check(String command) {
+		if (command.equals("1")) {
+			System.out.println(1);
+		}
+		else if (command.equals("2")) {
+			System.out.println(2);
+		}
+		else {
+			System.out.println("   Please enter a valid command");
+			System.out.println("Press return to continue");
+			try {
+				bReader.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			command = welcome();
+			check(command);
+		}
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hi :-)");
-
+		// setting up readers
+		System.out.println("===========  Welcome to the Dealership  =============\n\n");
+		String command1 = welcome();
+		check(command1);
+		
 	}
 
 }
---
+++ jmacias_project0/src/main/java/jmacias_project0/Login.java
@@ -0,0 +1,11 @@
+package jmacias_project0;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Login {
+	private static InputStreamReader iReader = new InputStreamReader(System.in);
+	private static BufferedReader bReader = new BufferedReader(iReader);
+	
+	
+}
