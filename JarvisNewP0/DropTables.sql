48c3b3f4e889cf3e7e88ddea8202c8abe18acc3f
JarvisAdams <jarvis.hi.adams@gmail.com>
2019-07-11 23:53:40 -0400

    Finished SQL table creation code, and verified that it created the tables in SQL Developer. There were several syntax issues overcome including requiring () after some of the data types, and the creation order when there are foreign key dependencies being defined at the same time as the table is created.
    Started development of an autoincrement sequence function on the cars
    table.

---
+++ JarvisNewP0/DropTables.sql
@@ -0,0 +1,6 @@
+DROP TABLE Contracts;
+DROP TABLE Payments;
+DROP TABLE Offers;
+DROP TABLE CarReges;
+DROP TABLE Cars;
+DROP TABLE Users;
--- JarvisNewP0/createTables.sql
+++ JarvisNewP0/createTables.sql
@@ -1,72 +1,65 @@
-CREATE TABLE Cars (
-CarID NUMBER,
-RegID NUMBER,
-OwnerID NUMBER,
-SpaceID NUMBER,
-OffersID NUMBER,
-ContractID NUMBER,
-PRIMARY KEY (CarID),
-CONSTRAINT FK_RegID FOREIGN KEY (RegID)
-REFERENCES Cars(RegID),
-CONSTRAINT FK_OwnerID FOREIGN KEY (OwnerID)
-REFERENCES Users(UserID),
-CONSTRAINT FK_OffersID FOREIGN KEY (OfferID)
-REFERENCES Offers(OfferID),
-CONSTRAINT FK_ContractID FOREIGN KEY (ContractID)
-REFERENCES Contracts(ContractID)
+CREATE TABLE Users (
+    UserDriversID NUMBER(16),
+    Password VARCHAR2(40),
+    Type INT,
+PRIMARY KEY (UserDriversID)
 );
 
-CREATE TABLE CarReges(
-RegID NUMBER,
-Make VARCHAR,
-Model VARCHAR,
-Year INT,
-MSRP FLOAT,
-CarID NUMBER,
+CREATE TABLE Cars (
+    CarID NUMBER(8),
+    RegID NUMBER(17),
+    OwnerID NUMBER(8),
+    SpaceID NUMBER(8),
+    OffersID NUMBER(10),
+    ContractID NUMBER(8),
 PRIMARY KEY (CarID),
-CONSTRAINT FK_Car FOREIGN KEY (CarID)
+CONSTRAINT FK_OwnerOfCar FOREIGN KEY (OwnerID)
+REFERENCES Users(UserDriversID)
+);
+
+CREATE TABLE CarReges (
+    RegID NUMBER(17),
+    Make VARCHAR2(40),
+    Model VARCHAR2(40),
+    Year INT,
+    MSRP FLOAT,
+    CarID NUMBER,
+PRIMARY KEY (CarID),
+CONSTRAINT FK_CarRegistration FOREIGN KEY (CarID)
 REFERENCES Cars(CarID)
 );
 
 CREATE TABLE Contracts (
-ContractID NUMBER,
-CarID NUMBER,
-SoldOffer FLOAT,
-Balance FLOAT,
-Terms INT,
+    ContractID NUMBER,
+    CarID NUMBER,
+    SoldOffer FLOAT,
+    Balance FLOAT,
+    Terms INT,
 PRIMARY KEY (ContractID),
-CONSTRAINT FK_Car FOREIGN KEY (CarID)
+CONSTRAINT FK_CarContract FOREIGN KEY (CarID)
 REFERENCES Cars(CarID)
 );
 
 CREATE TABLE Offers (
-OfferID NUMBER,
-Amount FLOAT,
-Term INT,
-APR FLOAT,
-OffereeID NUMBER,
-ProductID NUMBER,
+    OfferID NUMBER,
+    Amount FLOAT,
+    Term INT,
+    APR FLOAT,
+    OffereeID NUMBER,
+    ProductID NUMBER,
 PRIMARY KEY (OfferID,ProductID),
-CONSTRAINT FK_Offeree FOREIGN KEY (OffereeID)
-REFERENCES Users,
+CONSTRAINT FK_OffereeOffer FOREIGN KEY (OffereeID)
+REFERENCES Users(UserDriversID),
 CONSTRAINT FK_Product FOREIGN KEY (ProductID)
 REFERENCES Cars(CarID)
 );
 
 CREATE TABLE Payments (
-PaymentID NUMBER,
-ContractID NUMBER,
-Amount FLOAT,
-PayDate DATE,
+    PaymentID NUMBER,
+    ContractID NUMBER,
+    Amount FLOAT,
+    PayDate DATE,
 PRIMARY KEY (PaymentID,ContractID),
-CONSTRAINT FK_Contract FOREIGN KEY (ContractID)
+CONSTRAINT FK_ContractPayment FOREIGN KEY (ContractID)
 REFERENCES Contracts(ContractID)
 );
-
-CREATE TABLE Users (
-UserDriversID NUMBER,
-Password VARCHAR,
-Type BOOLEAN,
-PRIMARY KEY (UserDriversID)
-);
-
